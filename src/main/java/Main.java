1ff90c9cb8a39bdd76a33850635637d555607618
79056 <aleksandra.tikhomirova@onetwotrip.com>
2023-06-21 19:03:45 -0700

    AsosAuth: fix of comments + usage of test login credential

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,35 +3,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class Main {
     public static void main(String[] args){
-        //Запускаем драйвер
+        //Start the driver
         WebDriver driver = new ChromeDriver();
-        //Открываем нужный сайт
+        //Open required website
         driver.get("https://www.asos.com/us/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        //Переход на страницу авторизации, если страница полноразмерная
+
+        //Transition to authorization page. Variant with full screen mode.
         WebElement myAccountButton = driver.findElement(By.id("myAccountDropdown"));
         new Actions(driver).moveToElement(myAccountButton).perform();
         WebElement signInLink = driver.findElement(By.xpath("//a[@data-testid='signin-link']"));
         signInLink.click();
 
-        //Тут надо написать сценарий под разный масштаб страницы
+        //Develop later variant for narrow screen mode
 
-        //Авторизация по почте
+        //Authorization with email
         WebElement emailInput = driver.findElement(By.id("checkEmailAddress"));
-        emailInput.sendKeys("avtihomirova@mail.ru");
+        emailInput.sendKeys("apaccforautomation@gmail.com");
 
         WebElement continueButton = driver.findElement(By.className("continue-button"));
         continueButton.click();
 
         WebElement passwordInput = driver.findElement(By.id("Password"));
-        passwordInput.sendKeys("Autenti4n0");
+        passwordInput.sendKeys("0987654321ap!");
 
         WebElement signInButton = driver.findElement(By.id("signin"));
         signInButton.click();
